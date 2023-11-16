e28e0342e6e8a01fa8e7ee9d0a289f118f12d92c
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-11-16 07:33:08 +0300

    initial commit

---
+++ src/main/java/com/anas/geolocation/GeoLocationDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.anas.geolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocationDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoLocationDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anas/geolocation/GeoLocationDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anas.geolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoLocationDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
